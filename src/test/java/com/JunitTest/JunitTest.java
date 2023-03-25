dabe958de26fcb18fbc48ea5dd09bdf937d2b951
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-25 11:55:58 +0530

    Added : All emails should be pass using Parameterised Test cases.

--- src/test/java/com/JunitTest/JunitTest.java
+++ src/test/java/com/JunitTest/JunitTest.java
@@ -1,34 +1,52 @@
 package com.JunitTest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JunitTest {
     Junit runner = new Junit();
+
     @Test
-    void firstName(){
+    void firstName() {
         boolean output = runner.firstName("Abhishek");
-        assertEquals(true,output);
+        assertEquals(true, output);
     }
+
     @Test
-    void lastName(){
+    void lastName() {
         boolean output = runner.lastName("Magdum");
-        assertEquals(true,output);
+        assertEquals(true, output);
     }
+
     @Test
-    void validEmail(){
+    void validEmail() {
         boolean output = runner.validEmail("abhishek.magdum.07@gmail.com");
-        assertEquals(true,output);
+        assertEquals(true, output);
     }
+
     @Test
-    void phoneNumber(){
+    void phoneNumber() {
         boolean output = runner.phoneNumber("+91 7066987376");
-        assertEquals(true,output);
+        assertEquals(true, output);
     }
+
     @Test
-    void password(){
+    void password() {
         boolean output = runner.password("Abhi@1234");
-        assertEquals(true,output);
+        assertEquals(true, output);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net",
+                                  "abc.100@abc.com.au","abc@1.com","abc@gmail.com.com","abc+100@gmail.com"})
+    public void emails(String emails) {
+        boolean output = runner.validEmail(emails);
+        assertEquals(true, output);
     }
 }
+
+
+
